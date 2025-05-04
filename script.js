467c1aeca36e528dcb66740a295d3953948c5668
souvlaki <dpnminigames@gmail.com>
2025-05-04 17:28:13 +0200

    script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Souvlaki SMP Minecraft", /*Server name*/
-        serverName2: "Souvlaki SMP", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
-        serverIp: "souvlakismp.join-game.net", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverName2: "souvlakismp.join-game.net", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "Souvlaki SMP", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1011960565507293295" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
